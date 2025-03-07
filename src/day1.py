4b353c9cd5f31b703463e8d1dbd156ae503ae3f4
Emilio Oriwa Ogega <123199640+liohunter1@users.noreply.github.com>
2025-03-07 18:31:42 +0800

    Update day1.py
    Day 1: Learned variables and data types in Python"

--- src/day1.py
+++ src/day1.py
@@ -1,1 +1,8 @@
-print("Hello, Python 60 Days!") 
+# Learning Variables and Datatypes 
+# create variable name 
+#if its two words separate with a variable 
+character_name = "Emilio"
+character_age = "22"
+print("My name is " + character_name )
+print("I am  " + character_age + " years old")
+# makes it easy to modify data types 

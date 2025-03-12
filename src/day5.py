7bea95d8a2658c972e06975694d3eea21390ae06
Emilio Ogega <your-email@example.com>
2025-03-12 15:27:33 +0300

    did some work on functions and modularization

--- src/day5.py
+++ src/day5.py
@@ -1,3 +1,37 @@
 # Day 5: Python Practice
 
 print("Day 5 - Python 60 Days Challenge")
+# functions and modularization
+# A function is a block of code that performs a specific task when called.
+# Functions help in modularizing code, making it reusable and easier to read.
+#Task 1: Temperature Converter
+#Create a modular Python program to convert temperatures between Celsius and Fahrenheit.
+#converter.py
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+#Task 2 
+#Create a modular Python program that simulates basic bank account operations.
+#account.py
+class BankAccount:
+    def __init__(self, account_holder, account_number, balance=0):
+        self.account_holder = account_holder
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient funds"
+        self.balance -= amount
+        return self.balance
+
+    def get_balance(self):
+        return self.balance
+
+
--- src/day6.py
+++ src/day6.py
@@ -1,3 +1,29 @@
 # Day 6: Python Practice
 
 print("Day 6 - Python 60 Days Challenge")
+#main.py
+from day5 import celsius_to_fahrenheit, fahrenheit_to_celsius
+# Ask the user to choose the conversion type
+conversion_type = input("Choose the conversion type (Celsius to Fahrenheit or Fahrenheit to Celsius): ").lower().strip()
+#take inputs and perform the conversion and print the result
+if conversion_type == "celsius to fahrenheit":
+    celsius = float(input("Enter the temprature in Celcius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    print(f"{celsius}°C is equal to {fahrenheit}°F")
+elif conversion_type == "fahrenheit to celsius":
+    fahrenheit = float(input("Enter the temprature in Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit}°F is equal to {celsius}°C")
+else:
+    print("Invalid conversion type")
+    #Taccountmain.py
+from day5 import BankAccount
+#Create a bank account object
+account = BankAccount("Emilio", "123456")
+#Deposit some money
+account.deposit(5000)
+#Withdraw some money
+account.withdraw(2000)
+#Get the account balance
+balance = account.get_balance()
+print(f"The account balance is Ksh {balance}")

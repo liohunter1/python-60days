e581543d7ed299ed5d0eadf14d0510d58016f4f3
Emilio Ogega <your-email@example.com>
2025-03-12 15:36:27 +0300

    added docstrings to day5.py

--- src/day5.py
+++ src/day5.py
@@ -8,8 +8,10 @@ print("Day 5 - Python 60 Days Challenge")
 #Create a modular Python program to convert temperatures between Celsius and Fahrenheit.
 #converter.py
 def celsius_to_fahrenheit(celsius):
+    """Convert temperature from Celsius to Fahrenheit."""
     return (celsius * 9/5) + 32
 def fahrenheit_to_celsius(fahrenheit):
+    """Convert temperature from Fahrenheit to Celsius."""
     return (fahrenheit - 32) * 5/9
 
 #Task 2 
@@ -17,21 +19,25 @@ def fahrenheit_to_celsius(fahrenheit):
 #account.py
 class BankAccount:
     def __init__(self, account_holder, account_number, balance=0):
+        """ Initialize the BankAccount class with account holder name, account number, and balance."""
         self.account_holder = account_holder
         self.account_number = account_number
         self.balance = balance
 
     def deposit(self, amount):
-        self.balance += amount
+        """ Deposit money into the account."""
+        self.balance += amount # Add the amount to the balance
         return self.balance
 
     def withdraw(self, amount):
+        """ Withdraw money from the account."""
         if amount > self.balance:
             return "Insufficient funds"
         self.balance -= amount
         return self.balance
 
     def get_balance(self):
+        """ Get the current balance of the account."""
         return self.balance
 
 
